382cedc45988ca6638a54c6fc9f200e1a328a05e
Gary Tyler <mail@garytyler.com>
2019-05-10 21:57:51 -0400

    Fix redis dev config: use IN_MEMORY_CHANNEL_LAYER env var for dev else redis

--- eventvr_proj/settings_dev.py
+++ eventvr_proj/settings_dev.py
@@ -20,13 +20,8 @@ INSTALLED_APPS += ["debug_toolbar"]
 MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 
 # Channels
-in_memory = {"BACKEND": "channels.layers.InMemoryChannelLayer"}
-local_redis = {
-    "BACKEND": "channels_redis.core.RedisChannelLayer",
-    # "CONFIG": {"hosts": [("localhost", 6379)]},
-    "CONFIG": {"hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")]},
-}
-CHANNEL_LAYERS["default"] = local_redis if os.getenv("USE_REDIS") else in_memory
+if os.getenv("IN_MEMORY_CHANNEL_LAYER"):
+    CHANNEL_LAYERS["default"] = {"BACKEND": "channels.layers.InMemoryChannelLayer"}
 
 # Django debug toolbar
 DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda request: DEBUG}

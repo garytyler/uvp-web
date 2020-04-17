92f77bc7f7aec555908b761f34ea23bf5cdefaa1
Gary Tyler <mail@garytyler.com>
2020-04-17 11:02:10 -0400

    backend: Convert guest endpoint to class and add a test

--- backend/app/api/ws/endpoints/guest.py
+++ backend/app/api/ws/endpoints/guest.py
@@ -1,60 +1,48 @@
 import asyncio
 import logging
+from typing import Any
 
 from app.api.dependencies.publish import publish_feature
 from app.crud.features import crud_features
 from app.services.broadcasting import broadcast
 from fastapi import APIRouter, WebSocket, status
+from starlette.endpoints import WebSocketEndpoint
 
-log = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
 @router.websocket_route("/guest/{slug}")
-async def on_connect(websocket: WebSocket) -> None:
-    feature_slug = websocket.scope["path_params"].get("slug")
-    feature = await crud_features.get_by_slug(slug=feature_slug)
-    if not feature:
-        return await websocket.close(code=status.HTTP_404_NOT_FOUND)
-    await websocket.accept()
-    feature_id = feature.id
-    await publish_feature(id=feature_id)
+class WebsocketConsumer(WebSocketEndpoint):
+    async def on_connect(self, websocket: WebSocket) -> None:
+        # TODO Pass both feature id and guest id in route path and handle adding
+        # guest to feature guest list here, and use another endpoint for non-guest
+        # visitors.
 
-    guest_channel = str(feature.guest_channel)
+        feature_slug = websocket.scope["path_params"].get("slug")
+        feature = await crud_features.get_by_slug(slug=feature_slug)
+        if not feature:
+            return await websocket.close(code=status.HTTP_404_NOT_FOUND)
+        await websocket.accept()
+        feature_id = feature.id
+        await publish_feature(id=feature_id)
 
-    await run_loops(websocket=websocket, guest_channel=guest_channel)
+        self.guest_channel = str(feature.guest_channel)
 
-    await websocket.close()
+        self.guest_channel_subscriber_task = asyncio.create_task(
+            self.loop_send_guest_channel_to_client(websocket=websocket)
+        )
 
-
-async def loop_send_guest_channel_to_client(websocket, guest_channel):
-    async with broadcast.subscribe(channel=guest_channel) as subscriber:
-        async for event in subscriber:
+    async def loop_send_guest_channel_to_client(self, websocket):
+        async with broadcast.subscribe(channel=self.guest_channel) as subscriber:
+            async for event in subscriber:
+                await asyncio.sleep(0.01)
+                await websocket.send_text(event.message)
             await asyncio.sleep(0.01)
-            await websocket.send_text(event.message)
-        await asyncio.sleep(0.01)
 
+    async def on_receive(self, websocket: WebSocket, data: Any) -> None:
+        await broadcast.publish(channel=self.guest_channel, message=data)
 
-async def on_receive_from_client(websocket, guest_channel):
-    async for message in websocket.iter_text():
-        await broadcast.publish(channel=guest_channel, message=message)
-        await asyncio.sleep(0.01)
-    await asyncio.sleep(0.01)
-
-
-async def run_loops(websocket, guest_channel):
-    tasks = [
-        asyncio.create_task(
-            on_receive_from_client(websocket=websocket, guest_channel=guest_channel)
-        ),
-        asyncio.create_task(
-            loop_send_guest_channel_to_client(
-                websocket=websocket, guest_channel=guest_channel
-            )
-        ),
-    ]
-    (tasks_done, tasks_pending) = await asyncio.wait(
-        tasks, return_when=asyncio.FIRST_COMPLETED
-    )
-    [task.result() for task in tasks_done]
-    [task.cancel() for task in tasks_pending]
+    async def on_disconnect(self, websocket: WebSocket, close_code: int) -> None:
+        for task in getattr(self, "tasks", []):
+            task.cancel()
---
+++ backend/tests/ws/test_ws_guest_endpoints.py
@@ -0,0 +1,18 @@
+import json
+
+import pytest
+from tests._utils.features import create_random_feature
+from tests._utils.guests import create_random_guest
+
+
+@pytest.mark.asyncio
+async def test_guest_ws_broadcasts_feature_to_guests_on_connect(xclient):
+    async with xclient() as client:
+        random_feature = await create_random_feature()
+        await create_random_guest(feature=random_feature)
+        async with client.websocket_connect(f"/ws/guest/{random_feature.slug}") as ws1:
+            async with client.websocket_connect(f"/ws/guest/{random_feature.slug}"):
+                received_json = json.loads(await ws1.recv())
+                assert received_json["feature"]["id"] == str(random_feature.id)
+                assert received_json["feature"]["title"] == random_feature.title
+                assert received_json["feature"]["slug"] == random_feature.slug

d1129091458c55895a4649b23d581d7a65a48189
Gary Tyler <mail@garytyler.com>
2020-04-28 18:21:19 -0400

    Implement basic docker configuration.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,31 +3,35 @@ const { preserveFunctionNamesWithTerser } = require("typesafe-vuex/helpers");
 const fs = require("fs");
 
 module.exports = {
-  assetsDir: "static",
-  outputDir: "./dist/",
   configureWebpack: (config) => {
-    preserveFunctionNamesWithTerser(config);
-    config.devtool = "source-map";
-    const useSsl = ["SSL_KEY_FILE", "SSL_CERT_FILE"].every(
-      (v) => process.env[v] !== "null"
-    );
-    config.devServer = {
-      public: process.env.PUBLIC_HOSTNAME,
-      proxy: {
-        "/api/*": {
-          target: `${useSsl ? "https" : "http"}://${process.env.API_HOSTNAME}`,
-        },
-        "/ws/*": {
-          target: `${useSsl ? "wss" : "ws"}://${process.env.API_HOSTNAME}/`,
-          ws: true,
-        },
-      },
-    };
-    if (useSsl) {
-      config.devServer.https = {
-        key: fs.readFileSync(process.env.SSL_KEY_FILE),
-        cert: fs.readFileSync(process.env.SSL_CERT_FILE),
-      };
+    if (process.env.NODE_ENV === "production") {
+      preserveFunctionNamesWithTerser(config);
+    } else if (process.env.NODE_ENV === "development") {
+      // config.devtool = "source-map";
+      // const useSsl = ["SSL_KEY_FILE", "SSL_CERT_FILE"].every(
+      //   (v) => process.env[v] !== "null"
+      // );
+      // config.devServer = {
+      // public: process.env.PUBLIC_HOSTNAME,
+      // public: "0.0.0.0:8080",
+      //     proxy: {
+      //       "/api/*": {
+      //         target: `${useSsl ? "https" : "http"}://${
+      //           process.env.API_HOSTNAME
+      //         }`,
+      //       },
+      //       "/ws/*": {
+      //         target: `${useSsl ? "wss" : "ws"}://${process.env.API_HOSTNAME}/`,
+      //         ws: true,
+      //       },
+      //     },
+      //   };
+      //   if (useSsl) {
+      //     config.devServer.https = {
+      //       key: fs.readFileSync(process.env.SSL_KEY_FILE),
+      //       cert: fs.readFileSync(process.env.SSL_CERT_FILE),
+      //     };
+      // };
     }
   },
 };

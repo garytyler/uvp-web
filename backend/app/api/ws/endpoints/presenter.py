d3cbe92a27b7264c0ad78386425ef5edab98cff9
Gary Tyler <mail@garytyler.com>
2020-11-06 00:58:11 +0000

    🔥 backend: Remove crud class for presenter records

--- backend/app/api/ws/endpoints/presenter.py
+++ backend/app/api/ws/endpoints/presenter.py
@@ -5,8 +5,8 @@ from fastapi import APIRouter, WebSocket, status
 
 from app.api.dependencies.publish import publish_feature
 from app.core.redis import ChannelReader, redis
-from app.crud.presenters import crud_presenters
 from app.models.features import Feature
+from app.models.presenters import Presenter
 from app.schemas.presenters import PresenterCreate
 from app.utils.endpoints import APIWebSocketEndpoint
 
@@ -19,8 +19,8 @@ class PresenterWebSocket(APIWebSocketEndpoint):
         feature_slug = websockets.scope["path_params"].get("slug")
         if not (feature := await Feature.get_or_none(slug=feature_slug)):
             return await websockets.close(code=status.HTTP_404_NOT_FOUND)
-        presenter = await crud_presenters.create(
-            obj_in=PresenterCreate(feature_id=feature.id)
+        presenter = await Presenter.create(
+            **PresenterCreate(feature_id=feature.id).dict()
         )
         await presenter.save()
         await websockets.accept()

702bbc396a5c1aa280ff4742bcf6a60f59da5c84
Gary Tyler <mail@garytyler.com>
2020-11-18 21:55:27 -0500

    🔨 backend: Add manage.py development command cli

---
+++ backend/manage.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+
+import sys
+from importlib import import_module
+from inspect import getmembers, isclass
+
+import aioconsole
+import asyncclick as click
+from asgi_lifespan import LifespanManager
+
+from app.core.security import get_password_hash, verify_password
+from app.schemas.users import UserCreate
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+async def shell():
+    from app.core.db import TORTOISE_ORM
+    from app.main import app
+
+    repl_locals = {}
+    import_msgs = []
+    for app_dict in TORTOISE_ORM["apps"].values():
+        for module_dotpath in app_dict["models"]:
+            module_obj = import_module(module_dotpath)
+            for class_name, class_obj in getmembers(module_obj, isclass):
+                if module_dotpath in str(repr(class_obj)):
+                    repl_locals[class_name] = class_obj
+                    import_msgs.append(f"imported '{module_dotpath}.{class_name}'")
+    from app.core.config import get_settings
+
+    repl_locals["get_settings"] = get_settings
+    repl_locals["verify_password"] = verify_password
+    repl_locals["get_password_hash"] = get_password_hash
+    async with LifespanManager(app):
+        try:
+            await aioconsole.interact(banner="\n".join(import_msgs), locals=repl_locals)
+        except SystemExit:
+            sys.exit()
+
+
+@cli.command()
+async def runserver(host: str = "0.0.0.0", port: int = 8000):
+    pass
+
+
+@cli.command()
+@click.option("--email", "-e", required=True)
+@click.option("--password", "--pass", "-p", required=True)
+@click.option("--name", "-n")
+async def createsuperuser(email, password, name=None):
+    from app.main import app
+    from app.models.users import User
+
+    async with LifespanManager(app):
+        user_create = UserCreate(
+            name=name,
+            email=email,
+            hashed_password=get_password_hash(password),
+        )
+        user_obj = await User.create(**user_create.dict())
+
+    click.echo(f"Created superuser: {user_obj}")
+
+
+if __name__ == "__main__":
+    cli(_anyio_backend="asyncio")

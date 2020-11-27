4448172a5ada0e38ce46f09c610c87939c9e62b2
Gary Tyler <mail@garytyler.com>
2020-11-27 00:04:49 -0500

    ✅ frontend: Set default BASE_URL for e2e testing

--- frontend/tests/e2e/basic.spec.js
+++ frontend/tests/e2e/basic.spec.js
@@ -1,9 +1,11 @@
+BASE_URL = process.env.BASE_URL ? process.env.BASE_URL : "https://localhost";
+
 it("Home page", async () => {
-  await page.goto("https://localhost/");
+  await page.goto(`${BASE_URL}/`);
   await expect(page).toHaveText("[Home]");
 });
 
 it("non-existent feature is not found", async () => {
-  await page.goto("https://localhost/live/qpwomqpmviemg");
+  await page.goto(`${BASE_URL}/live/asdfasdf`);
   await expect(page).toHaveText("Feature not found.");
 });

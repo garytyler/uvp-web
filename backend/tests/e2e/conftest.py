cc3f28450b1658b6d2ed3d24db990dd131efdd02
Gary Tyler <mail@garytyler.com>
2021-01-09 16:07:55 -0500

    🛂 Borrow frontend target container's uid to call e2e tests from pytest

--- backend/tests/e2e/conftest.py
+++ backend/tests/e2e/conftest.py
@@ -1,7 +1,21 @@
+import tarfile
+from pathlib import Path
+
 import pytest
-from docker.models.containers import Container
 
 
 @pytest.fixture
-def frontend_container(docker_client) -> Container:
-    yield docker_client.containers.get("frontend")
+def retrieve_from_workdir(pytestconfig, tmp_path):
+    def _retrieve_from_workdir(container, rel_path):
+        archive_src = Path(container.attrs["Config"]["WorkingDir"]) / rel_path
+        archive_dest = tmp_path / rel_path
+        extract_dest = Path(pytestconfig.rootdir) / rel_path
+        stream, _ = container.get_archive(archive_src)
+        with open(archive_dest, "wb") as f:
+            for chunk in stream:
+                f.write(chunk)
+        with tarfile.open(archive_dest, "r") as f:
+            f.extractall(pytestconfig.rootdir)
+        return extract_dest
+
+    yield _retrieve_from_workdir
--- backend/tests/e2e/test_e2e.py
+++ backend/tests/e2e/test_e2e.py
@@ -1,31 +1,31 @@
 import uuid
 
-import docker
-
 
 class EndToEndTestError(Exception):
     pass
 
 
-def test_e2e_from_frontend(docker_client, frontend_container, request):
+def test_e2e_from_frontend(docker_client):
+    fe_container = docker_client.containers.get("frontend")
     try:
         pw_container = docker_client.containers.run(
             image="mcr.microsoft.com/playwright:focal",
             name=f"test-playwright-{uuid.uuid4()}",
             command=["npm", "run", "coverage:e2e"],
-            working_dir=frontend_container.attrs["Config"]["WorkingDir"],
-            network_mode=f"container:{frontend_container.id}",
-            volumes_from=frontend_container.id,
+            user=fe_container.exec_run("id -u").output.decode().strip(),
+            working_dir=fe_container.attrs["Config"]["WorkingDir"],
+            network_mode=f"container:{fe_container.id}",
+            volumes_from=fe_container.id,
             auto_remove=True,
             detach=True,
             stream=True,
             tty=False,
         )
-        out = ""
+        output = ""
         for line in pw_container.logs(stream=True, stdout=True, stderr=True):
-            out += line.decode()
+            output += line.decode()
         if 0 != pw_container.wait()["StatusCode"]:
-            raise EndToEndTestError(out)
+            raise EndToEndTestError(output)
     finally:
         try:
             pw_container.remove(force=True)

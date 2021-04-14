8b8efe48bc1f50da2fde6509f61b623720d51ca1
Gary Tyler <mail@garytyler.com>
2021-04-14 03:14:26 -0400

    ✅ backend: Use container db for tests instead of tortoise testing facilities

--- backend/tests/e2e/conftest.py
+++ backend/tests/e2e/conftest.py
@@ -1,33 +1,9 @@
 import copy
 import os
 import tarfile
-import uuid
 from pathlib import Path
 
 import pytest
-from docker.models.containers import Container
-
-
-@pytest.fixture(autouse=True)
-def pg_container(docker_client, settings) -> Container:
-    pg_prod_container = docker_client.containers.get("postgres")
-    pg_test_container_name = f"{os.environ['POSTGRES_DB']}-test-{uuid.uuid4()}"
-    pg_test_container = docker_client.containers.run(
-        name=pg_test_container_name,
-        image=pg_prod_container.image,
-        environment=dict(
-            POSTGRES_HOST=pg_test_container_name,
-            POSTGRES_USER=settings.DATABASE_URL.user,
-            POSTGRES_DB=settings.DATABASE_URL.path.strip("/"),
-            POSTGRES_HOST_AUTH_METHOD="trust",
-        ),
-        network=pg_prod_container.attrs["HostConfig"]["NetworkMode"],
-        detach=True,
-    )
-    try:
-        yield pg_test_container
-    finally:
-        pg_test_container.remove(v=True, force=True)
 
 
 @pytest.fixture(autouse=True)
--- backend/tests/unit/rest/conftest.py
+++ backend/tests/unit/rest/conftest.py
@@ -1,55 +1,28 @@
-import os
-import uuid
 from random import randint
 
 import pytest
 from asgi_lifespan import LifespanManager
-from tortoise.contrib.test import finalizer as tortoise_test_finalizer
-from tortoise.contrib.test import initializer as tortoise_test_initializer
 
-from app.core.db import get_tortoise_config
 from app.main import get_app
 
 
 @pytest.fixture(autouse=True)
-def db_suffix():
-    suffix = f"_test_{uuid.uuid4()}"
-    os.environ["DB_SUFFIX"] = suffix
-    yield suffix
-    del os.environ["DB_SUFFIX"]
-
-
-@pytest.fixture()
-def initialize_db():
-    tortoise_config: dict = get_tortoise_config()
-    tortoise_test_initializer(
-        modules=tortoise_config["apps"]["models"]["models"],
-        db_url=tortoise_config["connections"]["default"],
-        app_label="models",
-    )
-    yield
-    tortoise_test_finalizer()
-
-
-@pytest.fixture()
 @pytest.mark.asyncio
-async def app():
+async def app(pg_container):
     app = get_app()
-    async with LifespanManager(app):
+    async with LifespanManager(app, startup_timeout=20, shutdown_timeout=20):
         yield app
 
 
 @pytest.fixture(autouse=True)
 @pytest.mark.asyncio
-async def initial_user_objs(initialize_db, create_random_user_obj):
+async def initial_user_objs(app, create_random_user_obj):
     return [await create_random_user_obj() for _ in range(randint(5, 9))]
 
 
 @pytest.fixture(autouse=True)
 @pytest.mark.asyncio
-async def initial_feature_objs(
-    initialize_db, initial_user_objs, create_random_feature_obj
-):
+async def initial_feature_objs(app, initial_user_objs, create_random_feature_obj):
     feature_objs = []
     for user_obj in initial_user_objs:
         for _ in range(randint(1, 9)):
@@ -59,9 +32,7 @@ async def initial_feature_objs(
 
 @pytest.fixture(autouse=True)
 @pytest.mark.asyncio
-async def initial_guest_objs(
-    initialize_db, initial_feature_objs, create_random_guest_obj
-):
+async def initial_guest_objs(app, initial_feature_objs, create_random_guest_obj):
     guest_objs = []
     for feature_obj in initial_feature_objs:
         guest_objs = [
@@ -74,7 +45,7 @@ async def initial_guest_objs(
 @pytest.fixture(autouse=True)
 @pytest.mark.asyncio
 async def fetch_initial_relations(
-    initialize_db, initial_user_objs, initial_feature_objs, initial_guest_objs
+    app, initial_user_objs, initial_feature_objs, initial_guest_objs
 ):
     for i in initial_user_objs:
         await i.fetch_related("features")

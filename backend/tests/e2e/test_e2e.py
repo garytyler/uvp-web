0574d4e34cc7284a4727f60622bbc0688ab5b555
Gary Tyler <mail@garytyler.com>
2021-04-14 04:27:10 -0400

    ✅ backend: Programmatically sync playwright test image to library version

--- backend/tests/e2e/test_e2e.py
+++ backend/tests/e2e/test_e2e.py
@@ -8,18 +8,23 @@ class EndToEndTestError(Exception):
 
 
 def test_e2e_from_frontend(docker_client, pytestconfig, capsys):
-    fe_container = docker_client.containers.get("frontend")
-    fe_uid = fe_container.exec_run("id -u").output.decode().strip()
+    frontend_container = docker_client.containers.get("frontend")
+    frontend_uid = frontend_container.exec_run(["id", "-u"]).output.decode().strip()
+    playwright_lib_version = (
+        frontend_container.exec_run(["npm", "view", "playwright", "version"])
+        .output.decode()
+        .strip()
+    )
     output = ""
     try:
         pw_container = docker_client.containers.run(
-            image="mcr.microsoft.com/playwright:focal",
+            image=f"mcr.microsoft.com/playwright:v{playwright_lib_version}-focal",
             name=f"test-playwright-{uuid.uuid4()}",
             command=["npm", "run", "coverage:e2e"],
-            user=os.geteuid() if os.geteuid() == fe_uid else 0,
-            working_dir=fe_container.attrs["Config"]["WorkingDir"],
-            network_mode=f"container:{fe_container.id}",
-            volumes_from=fe_container.id,
+            user=os.geteuid() if os.geteuid() == frontend_uid else 0,
+            working_dir=frontend_container.attrs["Config"]["WorkingDir"],
+            network_mode=f"container:{frontend_container.id}",
+            volumes_from=frontend_container.id,
             ipc_mode="host",
             auto_remove=True,
             detach=True,
